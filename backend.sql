67a17e8e1cc47d86935ca7781acfc803db0c8e94
Niharika2211 <107017109+Niharika2211@users.noreply.github.com>
2024-12-27 15:06:41 +0530

    Update backend.sql

--- backend.sql
+++ backend.sql
@@ -7,20 +7,6 @@ CREATE TABLE IF NOT EXISTS transactions (
     description VARCHAR(255)
 );
 
-
-INSERT INTO transactions (amount, description)
-VALUES
-(700, 'Car maintenance'),
-(100, 'Movie tickets'),
-(250, 'Electronics purchase');
-
-
-
 CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
 GRANT ALL ON transactions.* TO 'expense'@'%';
 FLUSH PRIVILEGES;
-
-
-
-
-
